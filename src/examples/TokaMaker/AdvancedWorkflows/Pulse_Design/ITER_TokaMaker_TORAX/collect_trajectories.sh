58f691ef08a05d72a98ce3fe0595be38a366719f
Siddharth M. Bhatia <siddharth@sidmb.com>
2026-05-25 09:40:22 -0700

    Added collect_trajectories script for slurm

---
+++ src/examples/TokaMaker/AdvancedWorkflows/Pulse_Design/ITER_TokaMaker_TORAX/collect_trajectories.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+#SBATCH --account=nlp
+#SBATCH --gres=gpu:1
+#SBATCH --constraint=48G
+#SBATCH --mem=128G
+#SBATCH --partition=jag-standard
+#SBATCH --output=logs/%x-%j.out
+#SBATCH --error=logs/%x-%j.err
+
+# Optional setup
+# sh ./setup-env.sh
+
+uv run python collect_trajectories_delta.py --n_trajectories 1000 --start_idx 600 --output_dir ./rl_dataset_delta_sampling_maxloop=2_grid_51_run1
